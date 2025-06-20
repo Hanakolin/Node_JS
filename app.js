9852f9eefa7a96cfb0dfed9fa1621acaf4a8cc24
Hanakolin <bipana@greenpeacelincolncollege.edu.np>
2025-06-20 17:45:25 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express= require('express')
+const app=express()
+
+
+app.get('/',(req,res)=>{
+    res.send('bye World!')
+})
+
+ app.get('/about',(req, res)=>{
+    res.send('about page')
+ })
+
+app.listen(3000,()=>{
+    console.log ('start project')
+})
