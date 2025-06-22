eac69b01a537558aa6e80a15c9bc93b4def4c680
Hanakolin <bipana@greenpeacelincolncollege.edu.np>
2025-06-22 23:15:45 +0545

    file lai handle gara

--- app.js
+++ app.js
@@ -18,15 +18,23 @@ require('./model/index')
 app.use(express.urlencoded({ extended: true}))
 //yo line ko code important xa monolethic artecture ma xa vana Monolethic artecture vana ko databasse ra code  sanag vako wala 
 
- app.use(express.json())    //yo chai falto falto thou ma xa vana use hunxa hai 
+ app.use(express.json())  
+   //yo chai falto falto thou ma xa vana use hunxa hai
+
+
+    app.get ("/", async (req, res) =>{
+       const datas = await blogs.findAll()
+       res.render('home',{blogs :datas}) 
+    })
 
  app.get('/creat', (req, res) => {
     res.render('creat.ejs')
 })
 
 app.post('/creat', upload.single('image')  ,async (req, res) => {
+    const filename =req.file.filename
     const { title, subtitle, description } = req.body
-    await blogs.create({ title, subtitle, description })
+    await blogs.create({ title, subtitle, description,  image : filename})
     res.send('blog added successfully')
  })
 
@@ -34,6 +42,7 @@ app.post('/creat', upload.single('image')  ,async (req, res) => {
 
 
  app.use(express.static("public/css/")) 
+ app.use(express.static('./storage/'))
 
 app.listen(3000,()=>{
     console.log ('start project')
